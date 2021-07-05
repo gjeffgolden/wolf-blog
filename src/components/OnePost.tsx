cc6ad0000cbb5010f9aadd9ce859d20cda6b9d00
Jeff Golden <codebygolden@gmail.com>
2021-07-05 15:04:59 -0600

    Imported Tailwind and completed initial styling.

--- src/components/OnePost.tsx
+++ src/components/OnePost.tsx
@@ -72,25 +72,41 @@ export default function OnePost() {
     if (!postData) return <div>Loading...</div>
 
     return (
-        <div>
-            <div>
-                <h2>{postData.title}</h2>
-                <div>
-                    <img
-                        src={String(urlFor(postData.authorImage).width(100).url())}
-                        alt="Jeff Golden Author"
+        <div className="bg-gray-200 min-h-screen p-12">
+            <div className="container shadow-lg mx-auto bg-green-100 rounded-lg">
+                <div className="relative">
+                    <div className="absolute h-full w-full flex items-center justify-center p-8">
+                        <div className="bg-white bg-opacity-75 rounded p-12">
+                            <h2 className="cursive text-3xl lg:text-6xl mb-4">
+                                {postData.title}
+                            </h2>
+                            <div className="flex justify-center text-gray-800">
+                                <img
+                                    className="w-10 h-10 rounded-full"
+                                    src={String(urlFor(postData.authorImage).width(100).url())}
+                                    alt="Jeff Golden Author"
+                                />
+                                <h4 className="cursive flex items-center pl-2 text-2xl">
+                                    {postData.name}
+                                </h4>
+                            </div>
+                        </div>
+                    </div>
+                    <img 
+                        className="w-full object-cover rounded-t"
+                        src={String(urlFor(postData.mainImage).width(200).url())} 
+                        alt="Gray Wolf" 
+                        style={{ height: '400px'}}
                     />
-                    <h4>{postData.name}</h4>
+                </div>
+                <div className="px-16 lg:px-48 py-12 lg:py-20 prose lg:prose-xl max-w-full">
+                    <BlockContent
+                        blocks={postData.body}
+                        projectId={sanityClient.clientConfig.projectId}
+                        dataset={sanityClient.clientConfig.dataset}
+                    />
                 </div>
             </div>
-            <img src={String(urlFor(postData.mainImage).width(200).url())} alt="Gray Wolf" />
-            <div>
-                <BlockContent
-                    blocks={postData.body}
-                    projectId={sanityClient.clientConfig.projectId}
-                    dataset={sanityClient.clientConfig.dataset}
-                />
-            </div>
         </div>
-    )
+    );
 }
