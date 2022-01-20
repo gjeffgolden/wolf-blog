cb958014f0504ae9ee00231bf79c58d67cc72dfd
Jeff Golden <codebygolden@gmail.com>
2022-01-20 09:41:13 -0700

    Added tailwind config file and headshot to About Me

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,3 +1,12 @@
-const AboutMe = () => <div>About Me</div>
+import JeffGoldenHeadshot from "../assets/JeffGoldenHeadshot.jpg"
+
+const AboutMe = () => (
+  <main className="bg-gray-100 w-full">
+    <figure className="w-3/12">
+      <img src={JeffGoldenHeadshot} alt="Jeff Golden Headshot" />
+      <figcaption>Jeff Golden</figcaption>
+    </figure>
+  </main>
+)
 
 export default AboutMe
